8342e3333659feacba87a070f79e160dcb1298a9
MTianY <mtystar@qq.com>
2018-09-03 00:01:58 +0800

    block 的类型及各自在内存中的存储位置

--- block-2Part-1/block-2Part-1/main.m
+++ block-2Part-1/block-2Part-1/main.m
@@ -8,10 +8,81 @@
 
 #import <Foundation/Foundation.h>
 
+void test1() {
+    void(^testBlock1)(void) = ^{
+        NSLog(@"Hello World!");
+    };
+    
+    testBlock1();
+    
+    NSLog(@"%@",[testBlock1 class]);
+    NSLog(@"%@",[[testBlock1 class] superclass]);
+    NSLog(@"%@",[[[testBlock1 class] superclass] superclass]);
+    NSLog(@"%@",[[[[testBlock1 class] superclass] superclass] superclass]);
+}
+
+void test2() {
+    
+    void(^testBlock1)(void) = ^{
+        NSLog(@"testBlock1");
+    };
+    
+    int a = 10;
+    void(^testBlock2)(void) = ^{
+        NSLog(@"testBlock2---a = %d",a);
+    };
+    
+    NSLog(@"\ntestBlock1Class = %@\ntest2BlockClass = %@\nlastBlockClass = %@\n",[testBlock1 class], [testBlock2 class], [^{
+        NSLog(@"%d",a);
+    } class]);
+    
+}
+
+int b = 20;
+
+void test3() {
+    static int a = 10;
+    int c = 20;
+    void(^test3Block)(void) = ^{
+        NSLog(@"%d",c);
+    };
+    NSLog(@"%@",[test3Block class]);
+}
+
+void(^test4Block)(void);
+void test4 () {
+    int a = 30;
+//    void(^test4Block)(void) = ^{
+//        NSLog(@"a = %d",a);
+//    };
+    test4Block = [^{
+        NSLog(@"a = %d", a);
+    } copy];
+    
+    NSLog(@"%@",[test4Block class]);
+}
+
+void(^test5Block)(void);
+void test5() {
+    int a = 10;
+    test5Block = ^{
+        NSLog(@"a = %d",a);
+    };
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+//        test1();
+//        test2();
+//        test3();
+        
+//        test4();
+        
+        test5();
+        
+        test5Block();
+        
     }
     return 0;
 }
