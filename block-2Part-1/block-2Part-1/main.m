70651439706c24144de97ecbabd767c80ddf4acb
MTianY <mtystar@qq.com>
2018-09-02 17:50:41 +0800

    block-2Part-1 first commit

---
+++ block-2Part-1/block-2Part-1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  block-2Part-1
+//
+//  Created by 马天野 on 2018/9/2.
+//  Copyright © 2018年 Maty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
